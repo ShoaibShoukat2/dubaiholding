076dbbdfc801a920c3defbed06adf0640712dd58
Shoaib <shoaib@gmail.com>
2023-08-01 04:13:33 -0700

    dubai holding project

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return render(request,'about1.html')
+
+def contact(request):
+    return render(request,'contact.html')
